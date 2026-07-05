00d9a5f5bb2a078c0a2e23979689eaad08e84179
Hvalrdis <165889707+Hvalrdis@users.noreply.github.com>
2026-07-05 23:28:28 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Unified neural-network growth experiments."""
-
-from .experiment import ExperimentConfig, make_config, run_many, run_one
-
-__all__ = ["ExperimentConfig", "make_config", "run_many", "run_one"]
